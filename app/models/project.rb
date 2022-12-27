79b5790d1856bbdb4206c2446c4f4b56e76274e2
MarcosNBJ <marcosnery.comp@gmail.com>
2022-12-27 03:09:44 -0300

    feat: adding helper methods to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,10 +4,12 @@ class Project
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  belongs_to :state
+  embeds_many :stages
+
   field :owner_name, type: String
 
   field :address_cep, type: String
-  field :address_state, type: String
   field :address_city, type: String
   field :address_street, type: String
   field :address_neighborhood, type: String
@@ -20,4 +22,16 @@ class Project
 
   field :start_date, type: Date
   field :contract_date, type: Date
+
+  def price_per_meter
+    state.send("#{price_class}_price_per_meter")
+  end
+
+  def total_area
+    floor_sizes.sum
+  end
+
+  def total_cost
+    total_area * price_per_meter
+  end
 end
