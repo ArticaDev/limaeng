e8173ee9adee7b8f51792dc12c5a5648c208ea37
MarcosNBJ <marcosnery.comp@gmail.com>
2023-01-01 18:51:42 -0300

    feat: adding project budget generation

--- app/controllers/api/v1/budgets_controller.rb
+++ app/controllers/api/v1/budgets_controller.rb
@@ -6,11 +6,24 @@ module Api
       before_action :set_project, only: %i[show]
 
       def show
-        project_budget = Budget::GenerateProjectBudgetService.new(@project).call
+        project_budget = @project.last_generated_budget_hash
+        project_budget = generate_budget if project_budget.blank?
+
+        project_budget = project_budget.merge(
+          total_cost: @project.total_cost
+        )
+
         render json: project_budget, status: :ok
       end
 
       private
+
+      def generate_budget
+        project_budget = Budget::GenerateProjectBudgetService.new(@project).call
+        @project.update(last_generated_budget: project_budget.to_json)
+        GenerateProjectStagesService.new(@project, project_budget).call
+        project_budget
+      end
 
       def set_project
         @project = Project.find(params[:id])
---
+++ app/services/generate_project_stages_service.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class GenerateProjectStagesService
+  def initialize(project, project_budget)
+    @project = project
+    @project_budget = project_budget
+  end
+
+  def call
+    @project.stages = stage_types.map do |stage_type|
+      Stage.new(stage_type_id:stage_type.id.to_s, percentage_per_month:,
+                total_value: @project_budget[stage_type.name])
+    end
+    @project.save!
+  end
+
+  def percentage_per_month
+    Array.new(@project.duration_in_months, 0)
+  end
+
+  def stage_types
+    StageType.all
+  end
+end
