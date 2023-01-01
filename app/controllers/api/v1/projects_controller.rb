b648f5f2abb850db7e43efa120ed4ccce8f13f33
MarcosNBJ <marcosnery.comp@gmail.com>
2023-01-01 18:53:26 -0300

    feat: updating projects controller to display more info regarding monthly progress

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -11,11 +11,19 @@ module Api
       end
 
       def show
-        render json: @project
+
+        project_data = {
+          **@project.attributes.as_json.except('stages'),
+          current_month: current_month,
+          total_project_percentage: total_project_percentage,
+          latest_stages: stages_to_render,
+          months: months_array
+        }
+
+        render json: project_data
       end
 
       def create
-        @project = Project.create!(project_params)
         state = State.find_by!(abbreviation: params[:address_state])
         @project = Project.create!(project_params.merge(state:))
 
@@ -35,6 +43,71 @@ module Api
       end
 
       private
+
+      def months_array
+        expected_end_date = @project.start_date + (@project.duration_in_months-1).months
+
+        (@project.start_date..expected_end_date).map {|date|
+          date.strftime("%m-%y")
+        }.uniq
+      end
+
+      def current_month
+        (Date.today - @project.start_date).to_i / 30
+      end
+
+      def current_stages
+        stages = @project.stages 
+
+        return [] if stages.empty?
+
+        stages.select do |stage|
+          stage.percentage_per_month[current_month] > 0
+        end
+      end
+
+      def latest_stages
+        return [] if current_stages.empty?
+
+        current_stages.map do |stage|
+          {
+            name: stage.stage_type.name,
+            total_percentage: stage.current_total_percentage,
+            value: stage.total_value * stage.percentage_per_month[current_month] / 100
+          }
+        end
+      end
+
+      def first_stages
+        stages = @project.stages 
+
+        return [] if stages.empty?
+
+        first_stages = stages.select do |stage|
+          stage.percentage_per_month[0] > 0
+        end
+
+        first_stages.map do |stage|
+          {
+            name: stage.stage_type.name,
+            total_percentage: stage.current_total_percentage,
+            value: stage.total_value * stage.percentage_per_month[0] / 100
+          }
+        end
+      end
+
+      def total_project_percentage
+        
+        return 0 if current_stages.empty?
+
+        current_stages.map(&:current_total_percentage).sum
+      end
+
+      def stages_to_render
+        return latest_stages unless latest_stages.blank?
+
+        first_stages
+      end
 
       def set_project
         @project = Project.find(params[:id])
