7bf15bc9984a504b2d43b83cbe954e44e9082f2c
MarcosNBJ <marcosnery.comp@gmail.com>
2023-01-01 18:53:50 -0300

    feat: updating stages controller for stages crud

--- app/controllers/api/v1/stages_controller.rb
+++ app/controllers/api/v1/stages_controller.rb
@@ -3,7 +3,8 @@
 module Api
   module V1
     class StagesController < ApiController
-      before_action :set_project_and_stage, only: %i[update_stage]
+      before_action :set_project_and_stage, only: %i[update_stage stage]
+      before_action :set_project, only: %i[month stages_progression]
 
       def stage_types
         @stage_types = StageType.all
@@ -13,28 +14,95 @@ module Api
       def update_stage
         current_percentage_per_month = @stage.percentage_per_month
 
-        month = params[:month].to_i
+        month = params[:month]
         percentage = params[:percentage].to_f
+        
+        stage_index = (@project.start_date - Date.strptime(month,"%m-%y")).to_i/30
+        stage_index = stage_index.abs
 
-        current_percentage_per_month[month] = percentage
-        stage.percentage_per_month = current_percentage_per_month
-        stage.save!
+        current_percentage_per_month[stage_index] = percentage
+        @stage.percentage_per_month = current_percentage_per_month
+        @stage.save!
 
-        render json: stage, status: :ok
+        render json: @stage, status: :ok
       end
 
       def stage
-        stage_type_id = StageType.find_by(name: params[:stage_type]).id.to_s
-        @stage = @project.stages.find(stage_type_id:)
+        
         render json: @stage
       end
 
+      def month 
+        month = params[:month]
+        stage_index = (@project.start_date - Date.strptime(month,"%m-%y")).to_i/30
+        stage_index = stage_index.abs
+
+        total_cost = 0
+        month_stages = []
+
+        @project.stages.each {|stage|
+
+          next if stage.percentage_per_month[stage_index].zero?
+
+          percentage = stage.percentage_per_month[stage_index]
+
+          total_cost += stage.total_value * percentage / 100
+
+          month_stages << {
+            name: stage.stage_type.name,
+            percentage: percentage
+            
+          }
+
+        }
+
+        render json: {
+          total_cost: total_cost,
+          stages: month_stages
+        }
+
+      end
+
+      def stages_progression
+        stages = @project.stages
+
+        progression = stages.map {|stage|
+
+          next if stage.percentage_per_month.sum.zero?
+
+          {
+            name: stage.stage_type.name,
+            first_month: stage.percentage_per_month.index {|x| x != 0},
+            last_month: stage.percentage_per_month.rindex {|x| x != 0} + 1
+          }
+
+        }.compact
+
+        render json: { 
+          progression: progression,
+          months: months_array
+        }
+      end
+
+
       private
+
+      def months_array
+        expected_end_date = @project.start_date + (@project.duration_in_months-1).months
+
+        (@project.start_date..expected_end_date).map {|date|
+          date.strftime("%m-%y")
+        }.uniq
+      end
 
       def set_project_and_stage
         @project = Project.find(params[:id])
         stage_type_id = StageType.find_by(name: params[:stage_type]).id.to_s
-        @stage = @project.stages.find(stage_type_id:)
+        @stage = @project.stages.find_by(stage_type_id:)
+      end
+
+      def set_project
+        @project = Project.find(params[:id])
       end
     end
   end
