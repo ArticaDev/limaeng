ff453582082310de2e47d9c2d338914ef104e19a
MarcosNBJ <marcosnery.comp@gmail.com>
2023-10-28 03:29:53 -0300

    feat: adding localization

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Limaeng
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.default_locale = :"pt-BR" # or :pt depending on the specific locale file you have.
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
